7128070c0559ea37ed215a23c0c76f567f9ff6ca
Jon Bergerøy Ryssdal <jon@ryssdal.net>
2026-06-25 14:10:35 +0200

    nav update

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,82 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import * as bootstrap from "bootstrap";
 
 function Navbar() {
+  const navbarRef = useRef(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const closeNavbar = () => {
+    if (window.innerWidth < 992 && navbarRef.current) {
+      const collapse = bootstrap.Collapse.getOrCreateInstance(
+        navbarRef.current,
+      );
+      collapse.hide();
+    }
+  };
+
+  const scrollToSection = (sectionId) => {
+    closeNavbar();
+
+    if (location.pathname !== "/") {
+      navigate(`/#${sectionId}`);
+      return;
+    }
+
+    const section = document.getElementById(sectionId);
+
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
+  useEffect(() => {
+    if (location.hash) {
+      const sectionId = location.hash.replace("#", "");
+
+      setTimeout(() => {
+        const section = document.getElementById(sectionId);
+
+        if (section) {
+          section.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+      }, 100);
+    }
+  }, [location]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        window.innerWidth >= 992 ||
+        !navbarRef.current ||
+        !navbarRef.current.classList.contains("show")
+      ) {
+        return;
+      }
+
+      const toggler = document.querySelector(".navbar-toggler");
+
+      if (
+        !navbarRef.current.contains(event.target) &&
+        toggler &&
+        !toggler.contains(event.target)
+      ) {
+        closeNavbar();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg sticky-top">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/" onClick={closeNavbar}>
           Jon Bergerøy Ryssdal
         </Link>
 
@@ -21,38 +93,39 @@ function Navbar() {
         </button>
 
         <div
+          ref={navbarRef}
           className="collapse navbar-collapse justify-content-lg-center text-center"
           id="navbarNav"
         >
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/#about">
+              <a className="nav-link" href="/#about" onClick={closeNavbar}>
                 About
-              </Link>
+              </a>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/#education">
+              <a className="nav-link" href="/#education" onClick={closeNavbar}>
                 Education
-              </Link>
+              </a>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/#stack">
+              <a className="nav-link" href="/#stack" onClick={closeNavbar}>
                 Stack
-              </Link>
+              </a>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/#projects">
+              <a className="nav-link" href="/#projects" onClick={closeNavbar}>
                 Projects
-              </Link>
+              </a>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/#contact">
+              <a className="nav-link" href="/#contact" onClick={closeNavbar}>
                 Contact
-              </Link>
+              </a>
             </li>
           </ul>
         </div>
